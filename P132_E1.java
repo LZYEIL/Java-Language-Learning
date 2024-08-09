041afee860f5ca0f7b8f7209d9486ffe9fdadfd9
Zhiyuan Li <147009309+LZYEIL@users.noreply.github.com>
2024-08-09 14:51:49 +0800

    Create P132_E1.java

---
+++ P132_E1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i ++;
+        } while (i <= 100);
+
+
+
+         }
+
+    }
