546302b26ac8cc6559caf37a08dbb47d6082b34e
Zhiyuan Li <147009309+LZYEIL@users.noreply.github.com>
2024-08-09 13:59:34 +0800

    Create P129_E2.java

---
+++ P129_E2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+        int i = 40;
+        while (i < 200) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i ++;
+
+        }
+
+
+        }
+
+    }
