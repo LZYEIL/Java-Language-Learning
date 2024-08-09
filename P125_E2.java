0eafe34ccd364e30b91a1d04b3d2c828336b5851
Zhiyuan Li <147009309+LZYEIL@users.noreply.github.com>
2024-08-09 13:49:15 +0800

    Update P125_E2.java

--- P125_E2.java
+++ P125_E2.java
@@ -6,9 +6,10 @@ class Main{
 
         for (int i = 0; i <= 5; i++) {
             int j = 5 - i;
-            System.out.println(String.valueOf(i) + " + " + String.valueOf(j)
-                    + " = " + String.valueOf(i+j));
+            System.out.println(String.valueOf(i) + " + " + String.valueOf(j) //Note, here it is unnecessary to convert int to String
+                    + " = " + String.valueOf(i+j));                          //Java will automatically form them as a String object
             }
+        
 
         }
 
