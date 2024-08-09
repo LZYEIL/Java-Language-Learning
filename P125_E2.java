fc935c5c41a20211fa4bb4db73f60aec3743fcb8
Zhiyuan Li <147009309+LZYEIL@users.noreply.github.com>
2024-08-09 13:43:02 +0800

    Create P125_E2.java

---
+++ P125_E2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+
+        for (int i = 0; i <= 5; i++) {
+            int j = 5 - i;
+            System.out.println(String.valueOf(i) + " + " + String.valueOf(j)
+                    + " = " + String.valueOf(i+j));
+            }
+
+        }
+
+
+
+    }
